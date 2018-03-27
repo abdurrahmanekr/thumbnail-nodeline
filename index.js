d76eebfd21d6a868075de18059d8b03f8fa2a991
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2018-03-27 14:37:17 +0300

    some editings.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ rl.on('line', (line) => {
 	var heigth = 300, width, blur;
 	var fileName = line.trim();
 	if (type.length > 1) {
-		width = JSON.parse(type[0].split(':')[0]);
+	    width = JSON.parse(type[0].split(':')[0]);
 		heigth = JSON.parse(type[0].split(':')[1]);
 		fileName = type[1];
 		blur = JSON.parse(type[2] || '0');
@@ -24,12 +24,13 @@ rl.on('line', (line) => {
 		catronica = fs.readFileSync(__dirname + '/' + fileName);
 	} catch (e) {
 		console.log('!hata: dosya bulunamadı');
-		return;	
+		return;
 	}
-	sharp(catronica)
-        .resize(heigth, width)
-	.blur(blur)
-        .toFile(__dirname + '/output/' + fileName.split('/')[fileName.split('/').length -1], (err, info) => {
+	var file = sharp(catronica)
+        .resize(heigth, width);
+
+	if (blur) file = file.blur(blur);
+        file.toFile(__dirname + '/output/' + fileName.split('/')[fileName.split('/').length -1], (err, info) => {
 		if (err) {
 			console.log('!hata: dosya işlenemedi', err, info);
 		}
