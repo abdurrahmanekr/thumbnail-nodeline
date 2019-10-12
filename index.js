2969cc092a79137da211984ce556309becad21a5
Abdurrahman eker <abdurrahmaneker58@gmail.com>
2019-10-12 15:34:56 +0300

    yarn added, absolute path feat added.

--- index.js
+++ index.js
@@ -10,33 +10,38 @@ const rl = readline.createInterface({
 
 
 rl.on('line', (line) => {
-	var type = line.split(' ');
-	var heigth = 300, width, blur;
-	var fileName = line.trim();
-	if (type.length > 1) {
-	    width = JSON.parse(type[0].split(':')[0]);
-		heigth = JSON.parse(type[0].split(':')[1]);
-		fileName = type[1];
-		blur = JSON.parse(type[2] || '0');
-	}
-	var catronica;
-	try {
-		catronica = fs.readFileSync(__dirname + '/' + fileName);
-	} catch (e) {
-		console.log('!hata: dosya bulunamadı');
-		return;
-	}
-	var file = sharp(catronica)
+    var type = line.split(' ');
+    var heigth = 300, width, blur;
+    var fileName = line.trim();
+    if (type.length > 1) {
+        width = JSON.parse(type[0].split(':')[0]);
+        heigth = JSON.parse(type[0].split(':')[1]);
+        fileName = type[1];
+        blur = JSON.parse(type[2] || '0');
+    }
+    var catronica;
+    try {
+        if (fileName[0] === '/') {
+            catronica = fs.readFileSync(fileName);
+        }
+        else {
+            catronica = fs.readFileSync(__dirname + '/' + fileName);
+        }
+    } catch (e) {
+        console.log('!hata: dosya bulunamadı');
+        return;
+    }
+    var file = sharp(catronica)
         .resize(heigth, width);
 
-	if (blur) file = file.blur(blur);
+    if (blur) file = file.blur(blur);
         file.toFile(__dirname + '/output/' + fileName.split('/')[fileName.split('/').length -1], (err, info) => {
-		if (err) {
-			console.log('!hata: dosya işlenemedi', err, info);
-		}
-		else {
-			console.log(info);
-		}
+        if (err) {
+            console.log('!hata: dosya işlenemedi', err, info);
+        }
+        else {
+            console.log(info);
+        }
         });
 });
 
